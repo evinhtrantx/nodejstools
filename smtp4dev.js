46e51e942c6c25a6aab2bd02ab87f448ceefeced
vinht <vinht@omnix.com.au>
2019-06-06 12:24:38 +1000

    add smtp4dev test

--- smtp4dev.js
+++ smtp4dev.js
@@ -1,15 +1,20 @@
 const SMTPServer = require("smtp-server").SMTPServer;
 const fs = require("fs");
-const outfile = "C:/users/vinht/Documents/tmp/smtp1.txt";
-const output = fs.createWriteStream(outfile);
+var fileIndex = 0;
+const tmpData = "c:/tmp/smtp-"
 const localhost = new SMTPServer({
 	onData (stream,session,callback){
+		console.log("Data comming");
+		var outfile = tmpData + fileIndex + ".eml";
+		var output = fs.createWriteStream(outfile);
 		stream.pipe(output);
+		console.log("Done writing to:" + outfile);
+		fileIndex++;
 	},
 	
 	authOptional:true
 });
-localhost.listen(25,"localhost",function(){
-	console.log("listening on port 25");
+localhost.listen(2500,"localhost",function(){
+	console.log("listening on port 2500");
 	
 });
